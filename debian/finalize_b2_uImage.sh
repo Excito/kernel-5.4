c34027002fdbca596db21aa021ab3d7b34f5d0ea
Charles Leclerc <leclerc.charles@gmail.com>
2020-03-31 02:32:59 +0000

    Fix kernel uImage generation script

--- debian/finalize_b2_uImage.sh
+++ debian/finalize_b2_uImage.sh
@@ -4,7 +4,7 @@ cd $1
 
 UIMLEN=$(stat --printf="%s" uImage)
 UBOOTLOAD=$(( 0x00400000 ))
-KERNLENLOC=$(( 0x00700000 ))
+KERNLENLOC=$(( 0x01000000 ))
 PADDINGBYTES=$(( KERNLENLOC -  ( UBOOTLOAD + UIMLEN ) ))
 KERNELLEN=$(stat --printf="%s" vmlinux.bin)
 dd if=/dev/zero bs="${PADDINGBYTES}" count=1 status=none >> uImage
